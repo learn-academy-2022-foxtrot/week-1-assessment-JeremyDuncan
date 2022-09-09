f1442601954c0762be3419c70c9ef1e87afb0350
JeremyDuncan <jeremy.duncan1984@gmail.com>
2022-09-09 15:34:02 -0700

    finished question 1

--- coding-concepts.js
+++ coding-concepts.js
@@ -3,56 +3,74 @@
 // Examine the following examples.
 
 // First, predict the outcome based on your understanding of the code.
-// Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
+// Then, uncomment the console.log() and verify the output. Briefly explain why
+// your initial answer was correct or incorrect.
 
 // --------------------INSTRUCTOR EXAMPLE: What will this log?
-const colors = ["tangerine", "magenta", "lilac", "daffodil"]
+const colors = ["tangerine", "magenta", "lilac", "daffodil"];
 // console.log(colors.push("indigo"))
 
 // a) Your answer:
 // b) Verify and explain:
 
-
+//==============================================================================
 // --------------------1) What will this log?
+//==============================================================================
 
-const cohort = "LEARN 2022"
+const cohort = "LEARN 2022";
 // console.log(cohort.length)
 
 // a) Your answer:
-// b) Verify and explain:
+// expected output: 10
+// Explanation: This should be 10 since the .length() method counts not just the
+// characters, but the spaces inside a string too.
 
+// b) Verify and explain: according to:
+// https://www.w3schools.com/jsref/jsref_length_string.asp,
+// the length() method returns the length of the string.
+// according to: MDN Webdocs, length is also used to determine the length
+// the elements of an array.
+//-----------------------------
+// example:
+// const arr = [1, 2, 3, 4, 5]
+// arr.length // returns 5, the number of elements in the array
 
+//==============================================================================
 // --------------------2) What will this log?
+//==============================================================================
 
-const greeting = "Hello World!"
+const greeting = "Hello World!";
 // console.log(greeting[4])
 
 // a) Your answer:
 // b) Verify and explain:
 
-
+//==============================================================================
 // --------------------3) What will this log?
+//==============================================================================
 
-const languages = ["JavaScript", "Ruby", "Python", "C++"]
-const index = 1
+const languages = ["JavaScript", "Ruby", "Python", "C++"];
+const index = 1;
 // console.log(languages[index])
 
 // a) Your answer:
 // b) Verify and explain:
 
-
+//==============================================================================
 // --------------------4) What will this log?
+//==============================================================================
 
-const weekendDays = ["saturday", "sunday"]
+const weekendDays = ["saturday", "sunday"];
 // console.log(weekendDays.toUpperCase())
 
 // a) Your answer:
 // b) Verify and explain:
 
-
+//==============================================================================
 // --------------------5) What will this log?
+//==============================================================================
 
-const dataTypes = ["number", "string", "Boolean"]
+const dataTypes = ["number", "string", "Boolean"];
 // console.log(typeof dataTypes.length)
 
 // a) Your answer:
